ea42f11822ba35f3b0d6491c1ce775b738a9ef80
Brendon <55641029+unlmted@users.noreply.github.com>
2022-09-03 08:30:06 -0300

    Created dedicated move functions

--- main.py
+++ main.py
@@ -11,10 +11,30 @@ FPS = 60 # how many frames per second game updates at
 ALIEN = pygame.image.load(
         os.path.join('ASSETS', 'ALIEN.jpg'))
 ALIEN_RESIZE = pygame.transform.scale(ALIEN, (55, 55))
-
 HERO = pygame.image.load(
         os.path.join('ASSETS', 'HERO.jpg'))
 HERO_RESIZE = pygame.transform.rotate(pygame.transform.scale(HERO, (55, 55)), 115)
+VEL = 5 # Velocity (how much characters move)
+
+def hero_movement(keys_pressed, hero):
+    if keys_pressed[pygame.K_a]:  # left
+        hero.x -= VEL
+    if keys_pressed[pygame.K_d]:  # right
+        hero.x += VEL
+    if keys_pressed[pygame.K_w]:
+        hero.y -= VEL
+    if keys_pressed[pygame.K_s]:
+        hero.y += VEL
+
+def alien_movement(keys_pressed, alien):
+    if keys_pressed[pygame.K_UP]:
+        alien.y -= VEL
+    if keys_pressed[pygame.K_DOWN]:
+        alien.y += VEL
+    if keys_pressed[pygame.K_LEFT]:
+        alien.x -= VEL
+    if keys_pressed[pygame.K_RIGHT]:
+        alien.x += VEL
 
 def draw_window(hero, alien):
     WIN.fill(HOT_PINK)
@@ -31,26 +51,8 @@ def main():
         clock.tick(FPS) # Ensure we never go over this capped frame rate for performance
         draw_window(hero, alien) # Fill window color
         keys_pressed = pygame.key.get_pressed() # tells which keys are being pressed down
-
-        if keys_pressed[pygame.K_a]: # left
-            hero.x -= 1
-        elif keys_pressed[pygame.K_d]: # right
-            hero.x += 1
-        elif keys_pressed[pygame.K_w]:
-            hero.y -= 1
-        elif keys_pressed[pygame.K_s]:
-            hero.y += 1
-
-        if keys_pressed[pygame.K_u]:
-            alien.y -= 1
-        elif keys_pressed[pygame.K_j]:
-            alien.y += 1
-        elif keys_pressed[pygame.K_h]:
-            alien.x -= 1
-        elif keys_pressed[pygame.K_k]:
-            alien.x += 1
-
-
+        hero_movement(keys_pressed, hero)
+        alien_movement(keys_pressed, alien)
         # Gets a list of all different events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
